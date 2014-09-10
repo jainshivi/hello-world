439b1b2ffe3edcc378987f06d9a500893d9f6944
jainshivi <shivijain2807@gmail.com>
2014-09-09 23:11:59 -0700

    CMPE 273: Lab 1

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-This is my readme.
+Runs a simple RESTful server.
+
+
+How to run
+==========
+
+1. Build the code: "gradle build"
+2. Run the server: "gradle bootRun"
+
+
+
---
+++ src/main/scala/hello/HelloWorldController.scala
@@ -0,0 +1,18 @@
+package hello
+
+import org.springframework.boot._
+import org.springframework.boot.autoconfigure._
+import org.springframework.stereotype._
+import org.springframework.web.bind.annotation._
+
+import scala.collection.JavaConversions._
+
+
+@Controller
+@EnableAutoConfiguration
+class HelloWorldController {
+
+  @RequestMapping(Array("/"))
+  @ResponseBody
+  def home(): String = "Hello World!"
+}
